4ab1c7c05297adf75e699eea30aee2ad3bc9fd51
yfedoruck <yfedoruck@gmail.com>
2014-06-23 12:46:31 +0300

    --

--- .config/emacs/beginning-of-line.el
+++ .config/emacs/beginning-of-line.el
@@ -3,3 +3,16 @@
        (beginning-of-line)))
 
 (global-set-key (kbd "<home>") 'back-to-indentation-or-beginning)
+
+
+(defun back-to-indentation-or-beginning-select ()
+  ""
+  (interactive) ;; this is a command (i.e. can be interactively used)
+
+  (when (not (region-active-p))  ;; if the region is not active...
+    (push-mark (point) t t))     ;; ... set the mark and activate it
+
+  (back-to-indentation-or-beginning))               ;; move point ;; (backward-word)
+
+;; Bind the command to a key
+(global-set-key (kbd "S-<home>") 'back-to-indentation-or-beginning-select)
