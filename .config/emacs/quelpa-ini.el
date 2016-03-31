ce0613425b9ae9d58808a18beedd668a500d3a12
y.fedoruk <y.fedoruk@favbet.com>
2016-03-31 19:01:42 +0300

    --

--- .config/emacs/quelpa-ini.el
+++
@@ -1,5 +0,0 @@
-(if (require 'quelpa nil t)
-    (quelpa '(quelpa :repo "quelpa/quelpa" :fetcher github) :upgrade t)
-  (with-temp-buffer
-    (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
-    (eval-buffer)))
--- .config/emacs/reload-file.el
+++
@@ -1,7 +0,0 @@
-; refresh file f5
-(defun refresh-file ()
-  (interactive)
-  ;;(revert-buffer t t t)
-  (revert-buffer t (not (buffer-modified-p)) t)
-)
-(global-set-key [f5] 'refresh-file)
